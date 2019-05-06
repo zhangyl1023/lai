8ccae7b524915a1f0a7cac26b34480bcbe73ae85
zhangyl1023 <654928111@qq.com>
2019-05-06 23:04:51 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,12 @@
 function miao() {
+CONSOLE.log('ADFADGA')
+CONSOLE.log('ADFADGA')
+CONSOLE.log('ADFADGA')
+CONSOLE.log('ADFADGA')
+CONSOLE.log('ADFADGA')
+CONSOLE.log('ADFADGA')
     alert('miao');
+}
+
+function wu(){
 }
